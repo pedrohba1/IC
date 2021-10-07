f063bf49bab7dcd3de8f79f9fbcc8f97f2ee97be
Pedro Bufulin <pedro.almeida@ufu.br>
2021-10-06 21:41:50 -0300

    PSO discreto e híbrido

---
+++ Hybrid_discrete_PSO/Particles.py
@@ -0,0 +1,37 @@
+import numpy as np
+from AG_Functions import gen_individuals, darwinize_individual
+from Particle import Particle
+import random
+class Particles:
+    def __init__(self, N, n_queens):
+        self.N = N
+        self.board = np.array([x+1 for x in range(N*N)]).reshape(N,N)
+        self.particles = [] 
+        print('')
+        for queens in gen_individuals(n_queens,N,10):
+            score = darwinize_individual(queens, self.board)
+            p = Particle(queens,score, self.board, self.N)
+            self.particles.append(p)
+        self.rank_particles_by_actual_score()
+        self.max_particle = self.particles[0]
+
+
+    # a ideia dessa função é sempre manter na lista de partículas uma ordem decrescente de score: 
+    # a partícula de maior score atual está na posição 0 e a menor de todas na posição 100
+    def rank_particles_by_actual_score(self):
+        self.particles.sort(key=lambda p: p.actual_score , reverse=True)
+
+    def rank_particles_by_hig_score(self):
+        self.particles.sort(key=lambda p: p.high_score , reverse=True)
+
+    def do_iteration(self):
+        for idx, particle in enumerate(self.particles): 
+            # print('iteração ', idx)
+            if random.random() < 0.2:
+                particle.mutate_particle()
+            particle.move_particle(self.max_particle)
+            if particle.actual_score > self.max_particle.high_score:
+                self.max_particle = particle
+        # print('score máximo atual', self.max_particle.high_score)        
+
+
